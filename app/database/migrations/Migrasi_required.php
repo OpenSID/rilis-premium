d69e9c81aa2e55ae92c31cd119646b467bad954d
Abah Roland <59082428+vickyrolanda@users.noreply.github.com>
2026-01-06 15:47:32 +0700

    Rilis v2601.0.1

--- app/database/migrations/Migrasi_required.php
+++ app/database/migrations/Migrasi_required.php
@@ -11,7 +11,7 @@
  * Aplikasi dan source code ini dirilis berdasarkan lisensi GPL V3
  *
  * Hak Cipta 2009 - 2015 Combine Resource Institution (http://lumbungkomunitas.net/)
- * Hak Cipta 2016 - 2025 Perkumpulan Desa Digital Terbuka (https://opendesa.id)
+ * Hak Cipta 2016 - 2026 Perkumpulan Desa Digital Terbuka (https://opendesa.id)
  *
  * Dengan ini diberikan izin, secara gratis, kepada siapa pun yang mendapatkan salinan
  * dari perangkat lunak ini dan file dokumentasi terkait ("Aplikasi Ini"), untuk diperlakukan
@@ -29,13 +29,14 @@
  * @package   OpenSID
  * @author    Tim Pengembang OpenDesa
  * @copyright Hak Cipta 2009 - 2015 Combine Resource Institution (http://lumbungkomunitas.net/)
- * @copyright Hak Cipta 2016 - 2025 Perkumpulan Desa Digital Terbuka (https://opendesa.id)
+ * @copyright Hak Cipta 2016 - 2026 Perkumpulan Desa Digital Terbuka (https://opendesa.id)
  * @license   http://www.gnu.org/licenses/gpl.html GPL V3
  * @link      https://github.com/OpenSID/OpenSID
  *
  */
 
 use App\Traits\Migrator;
+use App\Actions\Setting\ImportSetting;
 use Illuminate\Database\Migrations\Migration;
 
 return new class () extends Migration {
@@ -47,6 +48,7 @@ return new class () extends Migration {
     public function up(): void
     {
         $this->tambah_ubah_surat_bawaan();
+        $this->tambah_ulang_pengaturan();
     }
 
     /**
@@ -62,5 +64,10 @@ return new class () extends Migration {
         $id = identitas('id');
         restoreSuratBawaanTinyMCE($id);
         restoreSuratBawaanDinasTinyMCE($id);
+    }
+    
+    public function tambah_ulang_pengaturan()
+    {
+        (new ImportSetting())->handle();
     }
 };
